796d5583cecca525a45292864896189c362acca8
mjw2705 <mjw2705@naver.com>
2021-02-19 16:23:07 +0900

    complete

--- baekjoon/baekjoon_15.py
+++ baekjoon/baekjoon_15.py
@@ -17,10 +17,48 @@ def greedy_11047():
 
 
 # 1931번
-num = int(input())
-time = [list(map(int, input().split())) for _ in range(num)]
-sort_time = sorted(time, key=lambda x: x[0])
+def greedy_1931():
+    num = int(input())
+    meet_time = [list(map(int, input().split())) for _ in range(num)]
+    sort_meet = sorted(meet_time, key=lambda x: (x[0], x[1]))
 
-print(sort_time)
-# if __name__ == '__main__':
+    # meet = [0 for _ in range(num)]
+    #
+    # for i in range(num):
+    #     cnt = 1
+    #     end = sort_meet[i][1]
+    #     for ti in sort_meet[i+1:]:
+    #         if end <= ti[0]:
+    #             cnt += 1
+    #             end = ti[1]
+    #     meet[i] = cnt
+    #
+    # print(max(meet))
+
+    end = 0
+    max_meet = 1
+    for ti in sort_meet:
+        if ti[0] >= end:
+            end = ti[1]
+            max_meet += 1
+
+    print(max_meet)
+
+
+# 11399번
+def greedy_11399():
+    num = int(input())
+    time = list(map(int, input().split()))
+
+    sort_time = sorted(time)
+
+    cnt = 0
+    for i in range(num):
+        cnt += sum(sort_time[i::-1])
+    print(cnt)
+
+
+if __name__ == '__main__':
     # greedy_11047()
+    # greedy_1931()
+    greedy_11399()
