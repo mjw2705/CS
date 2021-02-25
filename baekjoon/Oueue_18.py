26b0b75f9715f32f14c97def1b9a137d56e71dad
mjw2705 <mjw2705@naver.com>
2021-02-25 13:59:40 +0900

    add 1021

--- baekjoon/Oueue_18.py
+++ baekjoon/Oueue_18.py
@@ -18,7 +18,39 @@ def queue_11866():
 
 
 # 1021번 회전하는 큐
+def queue_1021():
+    n, m = map(int, sys.stdin.readline().split())
+    locate = list(map(int, sys.stdin.readline().split()))
+
+    num = [i+1 for i in range(n)]
+    cnt = 0
+
+    for i in range(m):
+        qu_len = len(num)
+        qu_idx = num.index(locate[i])
+
+        if qu_idx < qu_len // 2:
+            while True:
+                if num[0] == locate[i]:
+                    del num[0]
+                    break
+                else:
+                    num.append(num[0])
+                    del num[0]
+                    cnt += 1
+        else:
+            while True:
+                if num[0] == locate[i]:
+                    del num[0]
+                    break
+                else:
+                    num.insert(0, num[-1])
+                    del num[-1]
+                    cnt += 1
+
+    print(cnt)
 
 
-# if __name__ == '__main__':
-#     queue_11866()
+if __name__ == '__main__':
+    # queue_11866()
+    queue_1021()
