1112e0f29121258285ea47a831998e6a9f2df90b
mjw2705 <mjw2705@naver.com>
2021-05-04 20:00:20 +0900

    수식 최대화

--- Programmers/level2_expressionmax.py
+++ Programmers/level2_expressionmax.py
@@ -9,10 +9,10 @@ for expression in expressions:
     operator = []
     for exp in expression:
         if not exp.isdigit() and exp not in operator:
-            operator.append(exp)
+            operator.append(exp)  # ['*', '-']
 
     noperator = list(itertools.permutations(operator, len(operator)))
-    nums = re.split('(\D)', expression)
+    nums = re.split('(\D)', expression)  # ['50', '*', '6', '-', '3', '*', '2']
 
     answers = []
     for oper in noperator:
@@ -22,10 +22,9 @@ for expression in expressions:
             while op in num:
                 idx = num.index(op)
                 num[idx-1] = str(eval(num[idx-1] + num[idx] + num[idx+1]))
-                num = num[:idx] + num[idx+2:]
+                num = num[:idx] + num[idx+2:]  # ['300', '-', '3', '*', '2']
 
         answers.append(abs(int(num[0])))
 
     answer = max(answers)
-
     print(answer)
