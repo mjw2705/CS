1c65592a1b377bd2b44409ead173036a05a5b30e
mjw2705 <mjw2705@naver.com>
2021-05-14 00:40:40 +0900

    매뉴 리뉴얼

--- Programmers/level2_menurenew.py
+++ Programmers/level2_menurenew.py
@@ -1,10 +1,30 @@
+from collections import Counter
+from itertools import combinations
+
+# orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+# course = [2, 3, 4]
+orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+course = [2, 3, 5]
+
+
 def solution(orders, course):
     answer = []
+
+    for c in course:
+        combi = []
+        for order in orders:
+            combi += combinations(sorted(order), c)
+
+        count = Counter(combi)
+
+        if len(count) != 0 and max(count.values()) >= 2:
+            for coun in count:
+                if count[coun] == max(count.values()):
+                    answer.append(''.join(coun))
+
+    answer = sorted(answer)
+
     return answer
 
 
-orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
-course = [2, 3, 4]
-
-answer = []
-for order in orders:
+print(solution(orders, course))
