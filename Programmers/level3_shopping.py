929d8f512f3df27f0d126f04c87f8956314c08c8
mjw2705 <mjw2705@naver.com>
2021-06-07 19:41:04 +0900

    [1차] 보석 쇼핑

--- Programmers/level3_shopping.py
+++ Programmers/level3_shopping.py
@@ -5,11 +5,14 @@ gemss = [["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"],
         ["XYZ", "XYZ", "XYZ"],
         ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]]
 
+
+
 '''
 정확성: 15.6
 효율성: 4.4
 합계: 20.0 / 100.0'''
 # for gems in gemss:
+#     answer = []
 #     gem_set = set(gems)
 #     start, end = 0, 0
 #     bucket = {}
@@ -19,30 +22,26 @@ gemss = [["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"],
 #             bucket[gems[end]] = 1
 #         else:
 #             bucket[gems[end]] += 1
-#         if set(bucket) == gem_set:
-#             break
-#         else:
-#             end += 1
+#         end += 1
 #
-#     while start < len(gems):
-#         if gems[start] in bucket and bucket[gems[start]] != 1:
-#             bucket[gems[start]] -= 1
-#             start += 1
-#         else:
-#             break
-#     answer = [start+1, end+1]
+#         if set(bucket) == gem_set:
+#             while start < end:
+#                 if gems[start] in bucket and bucket[gems[start]] > 1:
+#                     bucket[gems[start]] -= 1
+#                     start += 1
+#                 else:
+#                     break
+#     answer = [start + 1, end]
 #     print(answer)
 
 
-'''
-정확성: 15.6
-효율성: 4.4
-합계: 20.0 / 100.0'''
+'''정답'''
 for gems in gemss:
     answer = []
     gem_set = set(gems)
     start, end = 0, 0
-    bucket = {}
+    bucket =  {}
+    shortest = len(gems) + 1
 
     while end < len(gems):
         if gems[end] not in bucket:
@@ -51,12 +50,15 @@ for gems in gemss:
             bucket[gems[end]] += 1
         end += 1
 
-        if set(bucket) == gem_set:
+        if len(gem_set) == len(bucket):
             while start < end:
                 if gems[start] in bucket and bucket[gems[start]] > 1:
                     bucket[gems[start]] -= 1
                     start += 1
+                elif shortest > end - start:
+                    shortest = end - start
+                    answer = [start+1, end]
+                    break
                 else:
                     break
-    answer = [start + 1, end]
     print(answer)
