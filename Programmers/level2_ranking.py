5bb90e647fc3cdc1571f98148ada5bf15ca9ab11
mjw2705 <mjw2705@naver.com>
2021-08-18 15:41:18 +0900

    순위 검색 효율성

--- Programmers/level2_ranking.py
+++ Programmers/level2_ranking.py
@@ -2,7 +2,7 @@ info = ["java backend junior pizza 150","python frontend senior chicken 210","py
 query = ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
 
 # 효율성 0%
-def solution(info, query):
+def solution1(info, query):
     answer = []
     for querys in query:
         count = 0
@@ -24,6 +24,64 @@ def solution(info, query):
                     count += 1
 
         answer.append(count)
+
+    return answer
+
+
+print(solution1(info, query))
+
+
+from itertools import combinations
+
+# 효율성 100%
+'''이분 탐색 필요'''
+def solution(info, query):
+    table = {}
+    answer = []
+
+    for i in info:
+        person = i.split(" ")
+
+        condition = person[:-1]
+        score = int(person[-1])
+
+        for n in range(5):
+            # print(list(combinations(condition, n)))
+            for c in combinations(condition, n):
+                temp = '/'.join(c)
+
+                if temp in table:
+                    table[temp].append(score)
+                else:
+                    table[temp] = [score]
+
+    # score 정렬
+    for value in table.values():
+        value.sort()
+
+
+    for q in query:
+        condi = q.replace(" and ", " ").split(" ")
+        q_score = int(condi[-1])
+        temp = condi[:-1]
+        while '-' in temp:
+            temp.remove('-')
+        q_query = '/'.join(temp)
+
+        if q_query in table:
+            score = table[q_query]
+
+            if len(score) > 0:
+                start, end = 0, len(score)
+                while start < end:
+                    mid = (start + end) // 2
+                    if score[mid] >= q_score:
+                        end = mid
+                    else:
+                        start = mid + 1
+                answer.append(len(score) - start)
+        else:
+            answer.append(0)
 
     return answer
 
