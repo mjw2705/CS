53fd54ba15bb4c599a3f80e7680bf8af4cdce4db
jiwon <mjw2705@naver.com>
2022-03-03 15:46:04 +0900

    짝지어 제거하기

---
+++ Programmers/level2/remove_pair.py
@@ -0,0 +1,19 @@
+ss = "baabaa", "cdcd"
+
+def solution(s):
+    stack = []
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
+
+for s in ss:
+    print(solution(s))
