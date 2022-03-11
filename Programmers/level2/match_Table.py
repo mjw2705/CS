8f4450ee98f163d8030d8bca27f26f2562040f92
jiwon <mjw2705@naver.com>
2022-03-11 13:34:46 +0900

    예상 대진표

---
+++ Programmers/level2/match_Table.py
@@ -0,0 +1,11 @@
+n, a, b = 8, 4, 7
+
+def solution(n, a, b):
+    answer = 0
+    while a != b:
+        answer += 1
+        a, b = (a+1) // 2, (b+1) // 2
+    return answer
+    
+print(solution(n, a, b))
+
