e739e7b9e2bc6ba4441d55823433334a7caaa6d1
jiwon <mjw2705@naver.com>
2022-07-28 14:15:10 +0900

    다시 풀이

--- Programmers/sorting/level1_kst_num.py
+++ Programmers/sorting/level1_kst_num.py
@@ -2,17 +2,13 @@ array = [1, 5, 2, 6, 3, 7, 4]
 commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
 
-answer = []
-for command in commands:
-    i, j, k = command
-    answer.append(sorted(array[i-1:j])[k-1])
-print(answer)
-
-
 def solution(array, commands):
     answer = []
     for command in commands:
         i, j, k = command
-        answer.append(sorted(array[i - 1:j])[k - 1])
-
+        part_arr = sorted(array[i-1:j])
+        answer.append(part_arr[k-1])
     return answer
+
+part = list(map(lambda x: sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+print(part)
